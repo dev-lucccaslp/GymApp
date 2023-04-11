73248de431e430e2e38a152e46658db84816a494
Lucas <luccaslp99@gmail.com>
2023-04-11 15:47:24 -0300

    completed header

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,40 @@
+import { HStack, Heading, VStack, Text, Icon } from "native-base";
+import { TouchableOpacity } from "react-native"
+
+import { MaterialIcons } from '@expo/vector-icons'
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems='center'>
+      <UserPhoto 
+        size={16}
+        alt="Imagem do usuário"
+        source={{uri: 'https://github.com/dev-lucccaslp.png'}}
+        mr={4}
+      />
+      
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize='md'>
+          Olá, 
+        </Text>
+
+        <Heading color="gray.100" fontSize='md'>
+          Lucas
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name="logout"
+          color='gray.200'
+          size={7}
+        />
+      </TouchableOpacity>
+
+        
+
+    </HStack>
+  )
+}
