6a717618406471798890602e9dd576a16c37f2e4
Lucas <luccaslp99@gmail.com>
2023-04-13 16:53:54 -0300

    Styling Invalid Inputs and Warnings

--- src/screens/SingUp.tsx
+++ src/screens/SingUp.tsx
@@ -16,7 +16,7 @@ type FormaDataProps = {
 }
 
 export function SignUp() {
-  const { control, handleSubmit } = useForm<FormaDataProps>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormaDataProps>();
   
   const navigation = useNavigation()
 
@@ -57,11 +57,16 @@ export function SignUp() {
           <Controller 
             control={control}
             name='name'
+            rules={{
+              required:'Informe o nome.'
+            }}
             render={({ field: { onChange, value }}) => (
               <Input
               placeholder='Nome'
               onChangeText={onChange}
               value={value}
+              errorMessage={errors.name?.message}
+              isInvalid
             />
             )}
           />
@@ -69,6 +74,13 @@ export function SignUp() {
           <Controller 
             control={control}
             name='email'
+            rules={{
+              required:'Informe o e-mail.',
+              pattern: {
+                value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido'
+              }
+            }}
             render={({ field: { onChange, value }}) => (
               <Input
                 placeholder='E-mail'
@@ -76,6 +88,7 @@ export function SignUp() {
                 autoCapitalize='none'
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
               />
             )}
           />
