51dc63edd78d44883ca229974e64a0ac6f87307d
Lucas <luccaslp99@gmail.com>
2023-04-13 08:15:59 -0300

    creating user profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,23 @@
-import { Center, Text } from "native-base";
+import { ScrollView, VStack, Center } from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color='white'>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil"/>
+
+      <ScrollView>
+        <Center mt={10} px={10}>
+          <UserPhoto
+            source={{uri: 'https://github.com/dev-lucccaslp.png'}}
+            alt="Foto do usuário"
+            size={33}
+          />
+
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
