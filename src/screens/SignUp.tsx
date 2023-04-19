783ca824c32abbb9e46dd31030eb0a01c1c0e5c3
Lucas <luccaslp99@gmail.com>
2023-04-19 08:32:44 -0300

    Axios Exceptions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,11 +4,15 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup';
 
+import { api } from '../service/api';
+import axios from 'axios';
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { Alert } from 'react-native';
 
 type FormaDataProps = {
   name: string;
@@ -36,19 +40,16 @@ export function SignUp() {
   }
 
   async function handleSignUp({name, email, password}: FormaDataProps) {
-    const response = await fetch('http://192.168.11.200:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password })
-    })
-      const data = await response.json();
-      console.log(data)
+    try {
+      const response= await api.post('/users', { name, email, password });
+      console.log(response.data)
 
-
-  
+    } catch(error) {
+      if(axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message)
+      }
+    }
+    
   }
 
   return (
