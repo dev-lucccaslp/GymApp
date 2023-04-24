860353bdc9a33ba5c0dd003af4f401e65f402baf
Lucas <luccaslp99@gmail.com>
2023-04-24 09:59:17 -0300

    Displaying the logged in user name

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,10 +1,14 @@
 import { HStack, Heading, VStack, Text, Icon } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons'
 import { TouchableOpacity } from "react-native"
 
-import { MaterialIcons } from '@expo/vector-icons'
 import { UserPhoto } from "./UserPhoto";
 
+import { useAuth } from "@hooks/useAuth";
+
 export function HomeHeader() {
+  const { user } = useAuth()
+  
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems='center'>
       <UserPhoto 
@@ -20,7 +24,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color="gray.100" fontSize='md'fontFamily='heading'>
-          Lucas
+          {user.name}
         </Heading>
       </VStack>
 
