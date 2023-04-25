67b8212acbd9d2fa5b59a0488f432bb4851e972f
Lucas <luccaslp99@gmail.com>
2023-04-25 08:58:41 -0300

    Using loading component on home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,16 +3,20 @@ import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { HStack,VStack, FlatList, Heading, Text, useToast } from "native-base";
 
 import { api } from "@services/api";
-import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { AppError } from "@utils/AppError";
+import { ExerciseDTO } from "@dtos/ExerciseDTO";
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { ExerciseCard } from "@components/ExerciseCard";
+import { Loading } from "@components/Loading";
 
 export function Home() {
+  const [ isLoading, setIsLoading ] = useState(true);
   const [ groups, setGroups ] = useState<string[]>([])
-  const [ exercises, setExercises ] = useState([]);
+  const [ exercises, setExercises ] = useState<ExerciseDTO[]>([]);
   const [ groupSelected, setGroupSelected ] = useState('costas');
 
   const toast = useToast();
@@ -41,8 +45,9 @@ export function Home() {
 
   async function fetchExercisesByGroup() {
     try {
+      setIsLoading(true);
       const response= await api.get(`/exercises/bygroup/${groupSelected}`)
-      console.log(response.data)
+      setExercises(response.data)
 
     } catch (error) {
       const isAppError = error instanceof AppError;
@@ -53,6 +58,8 @@ export function Home() {
         placement: 'top',
         bgColor: 'red.500'
       });
+    } finally {
+      setIsLoading(false);
     }
   } 
 
@@ -86,7 +93,10 @@ export function Home() {
         maxH={10}
         minH={10}
       />
-      <VStack flex={1} px={8}>
+      { 
+        isLoading ? <Loading /> :
+      
+        <VStack flex={1} px={8}>
         <HStack justifyContent='space-between' mb={5}>
           <Heading 
             color='gray.200'
@@ -105,15 +115,20 @@ export function Home() {
 
         <FlatList 
           data={exercises}
-          keyExtractor={item => item}
+          keyExtractor={item => item.id}
           renderItem={({item}) => (
-            <ExerciseCard onPress={handleOpenExerciseDetails} />
+            <ExerciseCard 
+              onPress={handleOpenExerciseDetails} 
+              data={item}
+            />
           )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{paddingBottom: 20}}
         />
 
-      </VStack>
+        </VStack>
+      
+      }
     </VStack>
   );
 }
